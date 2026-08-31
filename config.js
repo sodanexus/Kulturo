475f08d585e0dff24a05dd91990af357e2b0fc24
VIQ <pannetier.julien@gmail.com>
2026-08-31 23:44:09 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -38,7 +38,7 @@ const CONFIG = {
   // ── App settings ────────────────────────────────────────
   app: {
     name: "Kulturo",
-    version: "3.2.8",
+    version: "3.2.9",
     defaultTheme: "dark", // "dark" | "light"
     itemsPerPage: 24,
   },
